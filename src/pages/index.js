09ae3cefa12b53f1e763f5b45a450e290cc8f113
Zach Cohn <zcohn@wustl.edu>
2023-07-13 23:35:19 -0400

    deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
-import React, {useEffect, useState} from "react";
-
-
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/index.module.css";
 
 export default function Home() {
-
   const [isDead, setIsDead] = useState(false);
-  const [age, setAge] = useState(0);
+  const [age, setAge] = useState("");
   const [deathDate, setDeathDate] = useState("");
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     async function getDeadStatus() {
-      const response = await fetch("/api/deadStatus")
+      const response = await fetch("/api/deadStatus");
       const data = await response.json();
 
       setIsDead(data.is_dead);
@@ -19,22 +17,29 @@ export default function Home() {
       if (data.is_dead) {
         setDeathDate(data.death_date);
       }
+
+      setLoaded(true);
     }
     getDeadStatus();
   }, []);
 
-
   return (
     <div className={styles.container}>
-      <p className={styles.answer}>
-       {isDead ? "YES (sorry)" : "NOT YET"}
-      </p>
-      <p className={styles.age}>
-        {isDead ? `He was ${age} years old` : `He is ${age} years old`}
-      </p>
-      {isDead && <p className={styles.deathDate}>
-        He died on {deathDate}
-      </p>}
+      {loaded ? (
+        <>
+          <p className={styles.answer}>
+            {isDead ? "YES (sorry)" : "NOT YET"}
+          </p>
+          <p className={styles.age}>
+            {isDead ? `He was ${age} years old` : `He is ${age} years old`}
+          </p>
+          {isDead && (
+            <p className={styles.deathDate}>He died on {deathDate}</p>
+          )}
+        </>
+      ) : (
+        <p className={styles.loading}>Asking God...</p>
+      )}
     </div>
   );
 }
